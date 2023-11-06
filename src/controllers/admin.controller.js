141dc47461bb649d772e529209c87cf72596895f
SebastianAMo <sebastian.agudelo2@utp.edu.co>
2023-11-06 13:08:38 -0500

    change queries

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -36,7 +36,7 @@ const addColaborador = async (req, res) => {
 
 const getAllColaboradores = async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM colaboradores');
+    const result = await pool.query('SELECT * FROM colaboradores WHERE is_deleted = FALSE');
     res.json(result.rows);
   } catch (err) {
     res.status(500).send(err.message);
@@ -44,37 +44,40 @@ const getAllColaboradores = async (req, res) => {
 };
 
 const getColaboradorByNumId = async (req, res) => {
-    try {
-      const { numero_identificacion } = req.params;
-      const result = await pool.query('SELECT * FROM colaboradores WHERE numero_identificacion = $1', [numero_identificacion]);
-      res.json(result.rows[0]);
-    } catch (err) {
-
-      res.status(500).send(err.message);
-    }
+  try {
+    const { numero_identificacion } = req.params;
+    const result = await pool.query('SELECT * FROM colaboradores WHERE numero_identificacion = $1 AND is_deleted = FALSE', [numero_identificacion]);
+    res.json(result.rows[0]);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 };
   
 const updateColaborador = async (req, res) => {
   try {
     const { id, tipo_identificacion, numero_identificacion, nombre, apellido, fecha_nacimiento, estado_civil, sexo, direccion, telefono, correo_electronico, salario, fecha_ingreso, jerarquia, especialidad, usuario_id } = req.body;
+    const updateTimestamp = new Date();
     const result = await pool.query(
-      `UPDATE colaboradores SET tipo_identificacion = $1, numero_identificacion = $2, nombre = $3, apellido = $4, fecha_nacimiento = $5, estado_civil = $6, sexo = $7, direccion = $8, telefono = $9, correo_electronico = $10, salario = $11, fecha_ingreso = $12, jerarquia = $13, especialidad = $14, usuario_id = $15 WHERE id = $16 RETURNING *`,
-      [tipo_identificacion, numero_identificacion, nombre, apellido, fecha_nacimiento, estado_civil, sexo, direccion, telefono, correo_electronico, salario, fecha_ingreso, jerarquia, especialidad, usuario_id, id]
+      `UPDATE colaboradores 
+      SET tipo_identificacion = $1, numero_identificacion = $2, nombre = $3, apellido = $4, fecha_nacimiento = $5, estado_civil = $6, sexo = $7, direccion = $8, telefono = $9, correo_electronico = $10, salario = $11, fecha_ingreso = $12, jerarquia = $13, especialidad = $14, usuario_id = $15, updated_at = $17 
+      WHERE id = $16 AND is_deleted = FALSE
+      RETURNING *`,
+      [tipo_identificacion, numero_identificacion, nombre, apellido, fecha_nacimiento, estado_civil, sexo, direccion, telefono, correo_electronico, salario, fecha_ingreso, jerarquia, especialidad, usuario_id, id, updateTimestamp]
     );
     res.json(result.rows[0]);
   } catch (err) {
-
     res.status(500).send(err.message);
   }
 };
-  
+
+// Pensar en boorrado logico  
 const deleteColaborador = async (req, res) => {
   try {
     const { numero_identificacion } = req.params;
-    await pool.query('DELETE FROM colaboradores WHERE numero_identificacion = $1', [numero_identificacion]);
+    const deleteTimestamp = new Date();
+    await pool.query('UPDATE colaboradores SET is_deleted = $1, deleted_at = $2 WHERE numero_identificacion = $3', [true, deleteTimestamp, numero_identificacion]);
     res.json({ msg: 'Colaborador deleted' });
   } catch (err) {
-
     res.status(500).send(err.message);
   }
 };
@@ -82,9 +85,7 @@ const deleteColaborador = async (req, res) => {
 const patchColaborador = async (req, res) => {
   try {
     const { numero_identificacion } = req.params;
-    console.log(numero_identificacion);
     const keys = Object.keys(req.body).filter(key => key !== 'id');
-    console.log(keys);
     const values = keys.map(key => req.body[key]);
     const setString = keys.map((key, index) => `${key} = $${index + 1}`).join(', ');
 
---
+++ src/controllers/pacientes.controller.js
@@ -0,0 +1,94 @@
+const pool = require('../../config/dbConfig');
+
+const addPaciente = async (req, res) => {
+  try{
+    const { tipo_identificacion, numero_identificacion, nombre, apellido, fecha_nacimiento, estado_civil, sexo, direccion, telefono, correo_electronico, usuario_id } = req.body;
+
+    const result = await pool.query(
+      `INSERT INTO pacientes 
+        (tipo_identificacion, numero_identificacion, nombre, apellido, fecha_nacimiento, estado_civil, sexo, direccion, telefono, correo_electronico, usuario_id)
+      VALUES 
+        ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+      RETURNING *`,
+      [tipo_identificacion, numero_identificacion, nombre, apellido, fecha_nacimiento, estado_civil, sexo, direccion, telefono, correo_electronico, usuario_id]
+    );
+
+    res.status(201).json(result.rows[0]);
+      
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+const getAllPacientes = async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM pacientes WHERE is_deleted = FALSE');
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
+
+const getPacienteByNumId = async (req, res) => {
+  try {
+    const { numero_identificacion } = req.params;
+    const result = await pool.query('SELECT * FROM pacientes WHERE numero_identificacion = $1 AND is_deleted = FALSE', [numero_identificacion]);
+    res.json(result.rows[0]);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
+
+const updatePaciente = async (req, res) => {
+  try {
+    const { id, tipo_identificacion, numero_identificacion, nombre, apellido, fecha_nacimiento, estado_civil, sexo, direccion, telefono, correo_electronico, usuario_id } = req.body;
+    const updateTimestamp = new Date();
+    const result = await pool.query(
+      `UPDATE pacientes 
+      SET tipo_identificacion = $1, numero_identificacion = $2, nombre = $3, apellido = $4, fecha_nacimiento = $5, estado_civil = $6, sexo = $7, direccion = $8, telefono = $9, correo_electronico = $10, usuario_id = $11, updated_at = $13
+      WHERE id = $12 AND is_deleted = FALSE
+      RETURNING *`,
+      [tipo_identificacion, numero_identificacion, nombre, apellido, fecha_nacimiento, estado_civil, sexo, direccion, telefono, correo_electronico, usuario_id, id, updateTimestamp]
+    );
+    res.json(result.rows[0]);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+const deletePaciente = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTimestamp = new Date();
+    await pool.query('UPDATE pacientes SET is_deleted = $1, deleted_at = $2 WHERE id = $3', [true, deleteTimestamp, id]);
+    res.json({ msg: 'Paciente deleted logically' });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+const patchPaciente = async (req, res) => {
+  try {
+    const { numero_identificacion } = req.params;
+    const keys = Object.keys(req.body).filter(key => key !== 'id');
+    const values = keys.map(key => req.body[key]);
+    const setString = keys.map((key, index) => `${key} = $${index + 1}`).join(', ');
+
+    const query = `UPDATE pacientes SET ${setString} WHERE id = ${id} RETURNING *`;
+    const result = await pool.query(query, [...values, numero_identificacion]);
+    
+    res.json(result.rows[0]).status(200);
+
+  }catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+module.exports = {
+  addPaciente,
+  getAllPacientes,
+  getPacienteByNumId,
+  updatePaciente,
+  deletePaciente,
+  patchPaciente
+};
