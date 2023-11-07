48fed699d36975b3e86cf47bcd375e3ac89b09b2
SebastianAMo <sebastian.agudelo2@utp.edu.co>
2023-11-07 17:33:46 -0500

    add routes

--- src/routes/admin.router.js
+++ src/routes/admin.router.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
-const { getAllColaboradores,getColaboradorByNumId, addColaborador, updateColaborador, deleteColaborador, patchColaborador} = require('../controllers/admin.controller');
+const { getAllColaboradores,getColaboradorByNumId, getDeletedColaboradores, addColaborador, updateColaborador, deleteColaborador, patchColaborador} = require('../controllers/admin.controller');
 
-router.get('/colaboradores/', getAllColaboradores);
-router.get('/colaboradores/:numero_identificacion', getColaboradorByNumId);
 router.post('/colaboradores/', addColaborador);
+router.get('/colaboradores/', getAllColaboradores);
+router.get('/colaboradores/:numero_identificacion', getColaboradorByNumId); 
 router.put('/colaboradores/', updateColaborador);
 router.delete('/colaboradores/:numero_identificacion', deleteColaborador);
 router.patch('/colaboradores/:numero_identificacion', patchColaborador);
-
+router.get('/colaboradoresd', getDeletedColaboradores);
 
 module.exports = router;
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -5,17 +5,19 @@ const {
   getUserById,
   createUser,
   updateUser,
-  deleteUser,
+  disabledUser,
+  activeUser
 } = require('../controllers/user.controller');
 const authenticate = require('../middlewares/auth');
 const authorize = require('../middlewares/authorize');
 
-//router.delete('/:id', authenticate, authorize(['admin']), deleteUser);
+//router.delete('/:id', authenticate, authorize(['admin']), disabledUser);
 
 router.get('/', getUsers);
 router.get('/:id',  getUserById);
 router.post('/',  createUser);
 router.put('/:id',  updateUser);
-router.delete('/:id',  deleteUser);
+router.delete('/:id',  disabledUser);
+router.patch('/:id',  activeUser);
 
 module.exports = router;
