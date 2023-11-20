65fdc7b7d50c88a6f9cc4db66f674334d877dc15
SebastianAMo <sebastian.agudelo2@utp.edu.co>
2023-11-19 22:34:16 -0500

    delete line

--- src/routes/pacientes.router.js
+++ src/routes/pacientes.router.js
@@ -3,15 +3,10 @@ const router = express.Router();
 const {addPaciente,getAllPacientes,getPacienteByNumId,updatePaciente,deletePaciente,patchPaciente} = require('../controllers/pacientes.controller');
 
 router.post('/pacientes', addPaciente);
-
 router.get('/pacientes', getAllPacientes);
-
 router.get('/pacientes/:numero_identificacion', getPacienteByNumId);
-
 router.put('/pacientes/:numero_identificacion', updatePaciente);
-
 router.delete('/pacientes/:numero_identificacion', deletePaciente);
-
 router.patch('/pacientes/:numero_identificacion', patchPaciente);
 
 module.exports = router;
