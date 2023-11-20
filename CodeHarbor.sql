afa3ab23b685c26cc9c05ae4bd8b2d7257e64790
SebastianAMo <sebastian.agudelo2@utp.edu.co>
2023-11-20 12:27:25 -0500

    add foto_url

--- CodeHarbor.sql
+++ CodeHarbor.sql
@@ -126,47 +126,29 @@ CREATE TABLE IF NOT EXISTS blacklisted_tokens (
 
 
 COMMENT ON TABLE "users" IS 'Almacena información de los usuarios del sistema';
-
 COMMENT ON TABLE "colaboradores" IS 'Almacena información de los colaboradores de la clínica';
-
 COMMENT ON TABLE "pacientes" IS 'Almacena información de los pacientes de la clínica';
-
 COMMENT ON TABLE "medicamentos" IS 'Almacena información detallada de los medicamentos disponibles en la clínica, incluyendo si son de alto costo';
-
 COMMENT ON TABLE "formulas_medicas" IS 'Almacena información de las fórmulas médicas';
-
 COMMENT ON TABLE "medicamentos_recetados" IS 'Tabla de unión para relacionar fórmulas médicas con medicamentos';
-
 COMMENT ON TABLE "citas_medicas" IS 'Almacena información de las citas médicas';
-
 COMMENT ON TABLE "entrada_pacientes" IS 'Almacena información de la entrada de pacientes a la clínica';
-
 COMMENT ON TABLE "historias_clinicas" IS 'Contiene la información detallada de la historia clínica de cada paciente, incluyendo antecedentes, diagnósticos y tratamientos';
-
 COMMENT ON TABLE "solicitudes" IS 'Almacena información sobre las solicitudes de medicamentos de alto costo';
 
 ALTER TABLE "solicitudes" ADD FOREIGN KEY ("id_medicamento_recetado") REFERENCES "medicamentos_recetados" ("id");
-
 ALTER TABLE "historias_clinicas" ADD FOREIGN KEY ("id_Paciente") REFERENCES "pacientes" ("id");
-
 ALTER TABLE "historias_clinicas" ADD FOREIGN KEY ("id_Colaborador") REFERENCES "colaboradores" ("id");
-
 ALTER TABLE "colaboradores" ADD FOREIGN KEY ("usuario_id") REFERENCES "users" ("id");
-
 ALTER TABLE "pacientes" ADD FOREIGN KEY ("usuario_id") REFERENCES "users" ("id");
-
 ALTER TABLE "medicamentos_recetados" ADD FOREIGN KEY ("id_formula_medica") REFERENCES "formulas_medicas" ("id");
-
 ALTER TABLE "medicamentos_recetados" ADD FOREIGN KEY ("medicamento_id") REFERENCES "medicamentos" ("id");
-
 ALTER TABLE "formulas_medicas" ADD FOREIGN KEY ("id_paciente") REFERENCES "pacientes" ("id");
-
 ALTER TABLE "formulas_medicas" ADD FOREIGN KEY ("id_colaborador") REFERENCES "colaboradores" ("id");
-
 ALTER TABLE "citas_medicas" ADD FOREIGN KEY ("colaborador_id") REFERENCES "colaboradores" ("id");
-
 ALTER TABLE "citas_medicas" ADD FOREIGN KEY ("id_paciente") REFERENCES "pacientes" ("id");
-
 ALTER TABLE "entrada_pacientes" ADD FOREIGN KEY ("id_paciente") REFERENCES "pacientes" ("id");
-
 ALTER TABLE "entrada_pacientes" ADD FOREIGN KEY ("secretario_id") REFERENCES "colaboradores" ("id");
+
+ALTER TABLE "colaboradores" ADD COLUMN "foto_url" VARCHAR;
+ALTER TABLE "pacientes" ADD COLUMN "foto_url" VARCHAR;
