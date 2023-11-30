de2452977a5a05d4bb21319b185006e87927eafc
SebastianAMo <sebastian.agudelo2@utp.edu.co>
2023-11-29 21:20:00 -0500

    update

--- CodeHarbor.sql
+++ CodeHarbor.sql
@@ -94,8 +94,9 @@ CREATE TABLE "citas_medicas" (
   "id_cita" SERIAL PRIMARY KEY,
   "fecha" date,
   "hora" time,
-  "id_colaborador" integer,
+  "id_medico" integer,
   "id_paciente" integer,
+  "id_enfermero" integer,
   "eps" varchar,
   "estado" varchar,
   "motivo_consulta" text,
@@ -167,7 +168,8 @@ ALTER TABLE "medicamentos_recetados" ADD FOREIGN KEY ("id_formula_medica") REFER
 ALTER TABLE "medicamentos_recetados" ADD FOREIGN KEY ("id_medicamento") REFERENCES "medicamentos" ("id");
 ALTER TABLE "formulas_medicas" ADD FOREIGN KEY ("id_paciente") REFERENCES "pacientes" ("numero_identificacion");
 ALTER TABLE "formulas_medicas" ADD FOREIGN KEY ("id_colaborador") REFERENCES "colaboradores" ("numero_identificacion");
-ALTER TABLE "citas_medicas" ADD FOREIGN KEY ("id_colaborador") REFERENCES "colaboradores" ("numero_identificacion");
+ALTER TABLE "citas_medicas" ADD FOREIGN KEY ("id_medico") REFERENCES "colaboradores" ("numero_identificacion");
+ALTER TABLE "citas_medicas" ADD FOREIGN KEY ("id_enfermero") REFERENCES "colaboradores" ("numero_identificacion");
 ALTER TABLE "citas_medicas" ADD FOREIGN KEY ("id_paciente") REFERENCES "pacientes" ("numero_identificacion");
 ALTER TABLE "entrada_pacientes" ADD FOREIGN KEY ("id_paciente") REFERENCES "pacientes" ("numero_identificacion");
 ALTER TABLE "entrada_pacientes" ADD FOREIGN KEY ("secretario_id") REFERENCES "colaboradores" ("numero_identificacion");
